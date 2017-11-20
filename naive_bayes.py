919f5b338d69a718fa94e0f0f8313ba661a50018
Dai Dongyang <daidongyang12@gmail.com>
2017-11-21 01:19:56 +0800

    predict

--- naive_bayes.py
+++ naive_bayes.py
@@ -24,3 +24,34 @@ def train_single(subset):
     diff_sq = (subset - miu)**2
     sigma = np.sqrt(np.sum(diff_sq, axis=0)/n)
     return miu, sigma
+
+
+# todo: complete test
+def trains(samples, labels, c_list):
+    miu_m = list()
+    sigma_m = list()
+    for c in c_list:
+        idx = labels == c
+        subset = samples[idx, :]
+        miu, sigma = train_single(subset)
+        miu_m.append(miu)
+        sigma_m.append(sigma)
+    return np.array(miu_m), np.array(sigma_m)
+
+
+# todo: test
+def naive_bayes_predict(samples, c_list, miu_m, sigma_m, c_probs):
+    p_lists = list()
+    rows, _ = samples.shape
+    for i in range(len(c_list)):
+        # ln(p)
+        p = np.sum((samples - miu_m[i])**2/(2 * sigma_m[i]**2), axis=1) * np.log(1/c_probs[i])
+        p_lists.append(p)
+    p_matrix = np.array(p_lists)
+    idx_r = np.argmax(p_matrix, axis=1)
+    idx = zip(range(rows), idx_r)
+    c_matrix = np.array(c_list).repeat(rows).reshape(len(c_list), -1).transpose()
+    results = c_matrix(idx)
+    return results
+
+
--- naive_bayes_test.py
+++ naive_bayes_test.py
@@ -1,4 +1,6 @@
 import naive_bayes
+import load_data
+import pre_process
 import numpy as np
 
 
@@ -9,5 +11,15 @@ def test_train_single():
     print(sigma)
 
 
+def test_trains():
+    train_r_data, train_labels = load_data.load_data_labels(load_data.train_numbers)
+    test_r_data, test_labels = load_data.load_data_labels(load_data.test_numbers)
+    d = 5
+    train_data, test_data, loss = pre_process.pca_limit_d(train_r_data, test_r_data, d)
+    miu_m, sigma_m = naive_bayes.trains(train_data, train_labels, load_data.filter_l_number)
+    print(miu_m)
+    print(sigma_m)
+
 if __name__ == '__main__':
-    test_train_single()
+    test_trains()
+    # test_train_single()
