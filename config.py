2a706a1f2720bf9f31bc8c3c4fc47cbb48210eb7
Dai Dongyang <daidongyang12@gmail.com>
2017-11-23 02:07:40 +0800

    evaluate result

--- config.py
+++ config.py
@@ -1,12 +1,40 @@
 # configuration
+
+# the path of mat fold
 mats_path = './mats/'
+
+# the path of label fold
 labels_path = './labels/'
+
+# the original dimension of data before dimension reduction by PCA
 origin_dim = 1000
+
+# the list named 'train_numbers' is used to identify the training set
+# if the train_numbers is [1, 2, 3], the 'sleep_data_row3_1.mat', 'sleep_data_row3_2.mat', 'sleep_data_row3_3.mat' and
+# 'HypnogramAASM_subject1.txt', 'HypnogramAASM_subject2.txt', 'HypnogramAASM_subject3.txt' will be treat as train set
+# and train set
 train_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]  # list(range(1,20))
+
+# the list named 'test_numbers' is used to identify the test set
 test_numbers = [20]
+
+# the class be considered, if considered_classes = [1, 2, 3], we will only consider data corresponds to classes 1, 2, 3
 considered_classes = [1, 2, 3]
-freq_k = 1
-d = 5
-fs = 200
+
 is_filter_test = True
+
+# the k main frequency to be considered, if freq_k = 3, we consider the frequency with the top 3 highest energy,
+# if freq_k = 1, we only consider the primary frequency with the highest energy
+freq_k = 1
+
+# Sampling rate
+fs = 200
+
+# The target dimension to reduce for PCA process
+d = 5
+
+# The name of file to save predict result
 result_file = 'result.txt'
+
+# The name of file to save ground truth
+gt_file = 'ground_truth.txt'
--- main.py
+++ main.py
@@ -4,6 +4,40 @@ import load_data
 import pre_process
 import numpy as np
 import time
+from collections import defaultdict
+
+
+def result_evaluate(g_ls, r_ls, c_list):
+    g_ls = g_ls.reshape(-1, 1)
+    r_ls = r_ls.reshape(-1, 1)
+    check = np.array(g_ls == r_ls)
+    total_num = g_ls.size
+    acc = np.sum(check) / total_num
+
+    g_ls = list(g_ls.flat)
+    r_ls = list(r_ls.flat)
+    tps_dict = defaultdict(lambda: 0)
+    positives_dict = defaultdict(lambda: 0)
+    trues_dict = defaultdict(lambda: 0)
+    for i in range(total_num):
+        positives_dict[r_ls[i]] += 1
+        trues_dict[g_ls[i]] += 1
+        if r_ls[i] == g_ls[i]:
+            tps_dict[r_ls[i]] += 1
+    positives = np.zeros(len(c_list))
+    trues = np.zeros(len(c_list))
+    tps = np.zeros(len(c_list))
+    for i in range(len(c_list)):
+        positives[i] = positives_dict[c_list[i]]
+        trues[i] = trues_dict[c_list[i]]
+        tps[i] = tps_dict[c_list[i]]
+    pre = tps/positives
+    rec = tps/trues
+    F1 = (2 * pre * rec)/(pre + rec)
+    macro_pre = np.average(pre)
+    macro_rec = np.average(rec)
+    macro_F1 = (2 * macro_pre * macro_rec)/(macro_pre + macro_rec)
+    return acc, pre, rec, F1, macro_pre, macro_rec, macro_F1
 
 
 if __name__ == '__main__':
@@ -41,17 +75,23 @@ if __name__ == '__main__':
     t10 = time.time()
     print('Test time: ', t10 - t9, 's')
 
-    check = results == test_labels
-    acc = np.sum(check) / len(check)
-    t11 = time.time()
-    # print(acc)
-    for i in check:
-        print(i)
-    # print(check)
     with open(config.result_file, 'w') as outf:
         print('predict result:', file=outf)
         for result in results:
             print(result, file=outf)
 
+    with open(config.gt_file, 'w') as outf:
+        print('ground truth:', file=outf)
+        for test_label in test_labels:
+            print(test_label, file=outf)
+
+    acc, pre, rec, F1, macro_pre, macro_rec, macro_F1 = result_evaluate(test_labels, results, config.considered_classes)
+    t11 = time.time()
     print('Total time: ', t11 - t1, 's')
     print('Accuracy is', acc)
+    print('precision of', config.considered_classes, 'is:', pre)
+    print('recall of', config.considered_classes, 'is:', rec)
+    print('F1 of', config.considered_classes, 'is:', F1)
+    print('Macro-precision is:', macro_pre)
+    print('Macro-recall is:', macro_rec)
+    print('Macro-F1 is:', macro_F1)
