62cf1135f2ed3e254585bd75ed8736470b86c846
Dai Dongyang <daidongyang12@gmail.com>
2017-11-23 20:15:36 +0800

    comment pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,15 +1,25 @@
+"""
+Preprocessing data before classification
+"""
 import scipy.io as sio
+import time
 import numpy as np
 
 
-# fft transform to get k main frequencys
 def get_k_freqs(set, fs, k):
+    """
+    get k main frequencies with the top kth maximum amplitude by fft transform
+    :param set: data to process, a m*n matrix meaning m instances and every instance has n features
+    :param fs: Sampling Rate
+    :param k: the number of main frequencies to return
+    :return:
+        k_freqs: the k main frequencies with the top kth maximum amplitude
+        the last two element to return is the entire information of Spectrum
+    """
     rows, N = set.shape
     f = fs * np.arange(1, N+1)/N
     S_double = np.fft.fft(set, n=N, axis=1)
     S_single_abs = np.abs(S_double[:, 0: int(N/2)])
-
-    # maybe this command takes too much time
     S_single_argpart = np.argsort(S_single_abs, axis=1)
     k_max_argpart = S_single_argpart[:, : -1*k-1: -1]
     idx = (np.arange(rows)*N).reshape(-1, 1) + k_max_argpart
@@ -21,6 +31,16 @@ def get_k_freqs(set, fs, k):
 
 
 def pca_limit_d(train_set, test_set, d):
+    """
+    reduce dimension with specifying target dimension by PCA
+    :param train_set: training set, m*n matrix which means m training instances and every instance has n old features
+    :param test_set: test set, k*n matrix which means k test instances and every instance has n old features
+    :param d: the target dimension to reduce
+    :return:
+        trains: new training set, m*d matrix
+        tests: new test set, k*d matrix
+        loss: the loss rate of the sum of eigenvalues
+    """
     _, old_d = train_set.shape
     if d >= old_d:
         return train_set, test_set, 0
@@ -39,6 +59,16 @@ def pca_limit_d(train_set, test_set, d):
 
 
 def pca_limit_loss(train_set, test_set, loss):
+    """
+    reduce dimension with specifying the loss rate of the sum of eigenvalues by PCA
+    :param train_set: training set, m*n matrix which means m training instances and every instance has n old features
+    :param test_set: test set, k*n matrix which means k test instances and every instance has n old features
+    :param loss: the loss rate of the sum of eigenvalues
+    :return:
+        trains: new training set, m*d matrix
+        tests: new test set, k*d matrix
+        loss: the target dimension to reduce
+    """
     miu = np.average(train_set, axis=0)
     avg_trains = train_set - miu
     cov = np.cov(avg_trains.T)
@@ -59,7 +89,3 @@ def pca_limit_loss(train_set, test_set, loss):
     trains = avg_trains.dot(M)
     tests = np.dot((test_set - miu), M)
     return trains, tests, d
-
-
-def concatenate_features(set1, set2):
-    return np.hstack(set1, set2)
